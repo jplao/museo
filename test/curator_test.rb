8d9e319635dc52677d43d86b5c90a031b085a0c1
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-18 10:05:08 -0600

    adds find photograph by artist and tests

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -125,4 +125,68 @@ class CuratorTest < Minitest::Test
     assert_instance_of Photograph, actual
     assert_equal "Moonrise, Hernandez", actual.name
   end
+
+  def test_it_can_find_photographs_by_artist
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    curator = Curator.new
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+
+    diane_arbus = curator.find_artist_by_id("3")
+    actual = curator.find_photographs_by_artist(diane_arbus)
+    assert_instance_of Array, actual
+    assert_instance_of Photograph, actual.first
+    assert_equal "3", actual.first.artist_id
+    assert_equal "Identical Twins, Roselle, New Jersey", actual.first.name
+  end
 end
