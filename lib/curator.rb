4e7efaef40d3a37d77beca26db48c696c75f137f
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-18 10:28:58 -0600

    adds find artist from country method and tests

--- lib/curator.rb
+++ lib/curator.rb
@@ -42,4 +42,24 @@ class Curator
       artist_photos.count > 1
     end
   end
+
+  def photographs_taken_by_artists_from(country)
+    artist_ids = find_artist_ids_from_country(country)
+    @photographs.find_all do |photograph|
+      artist_ids.include?(photograph.artist_id)
+    end
+  end
+
+  def find_artists_from_country(country)
+    @artists.find_all do |artist|
+      artist.country == country
+    end
+  end
+
+  def find_artist_ids_from_country(country)
+    artists = find_artists_from_country(country)
+    artists.map do |artist|
+      artist.id
+    end
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -254,4 +254,67 @@ class CuratorTest < Minitest::Test
     assert_equal 1, actual.length
     assert_equal true, diane_arbus == actual.first
   end
+
+  def test_it_can_find_photographs_based_on_artist_country
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    curator = Curator.new
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+
+    actual = curator.photographs_taken_by_artists_from("United States")
+    assert_instance_of Array, actual
+    assert_instance_of Photograph, actual[0]
+    assert_equal "Moonrise, Hernandez", actual.first.name
+    assert_equal [], curator.photographs_taken_by_artists_from("Argentina")
+  end
 end
